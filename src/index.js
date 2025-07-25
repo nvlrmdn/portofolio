d0a4e6e39237e30ca4de88c2110caacb41e91cde
Noval Ramdani <noval.jkt1.ebdesk.com>
2025-07-26 02:34:17 +0700

    Setup GitHub Actions for deploy

--- src/index.js
+++ src/index.js
@@ -209,3 +209,16 @@ new Chart(ctx, {
     }
   }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      e.preventDefault();
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
